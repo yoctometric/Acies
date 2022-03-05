8968bed996e19a31752fdf8d1260364ac78ee2c5
yoctometric <willcunningham64@gmail.com>
2022-03-05 12:52:08 -0500

    began adding buttons to toolbar

--- dev/main.py
+++ dev/main.py
@@ -24,14 +24,40 @@ hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275)
 
 # defines the toolbar ui layout
 class Toolbar:
-    def __init__(self, manager) -> None:
+    def __init__(self, manager, height) -> None:
+        self.panel_rect = pygame.Rect(0, SCREEN_DIMENSIONS[1] - height, SCREEN_DIMENSIONS[0], height)
+
+        # this statement defines and passes the panel to the ui manager
         self.panel = pygame_gui.elements.UIPanel(
-                    relative_rect=pygame.Rect(0, SCREEN_DIMENSIONS[1] - 100, SCREEN_DIMENSIONS[0], 100), 
+                    relative_rect=self.panel_rect, 
                     manager=manager, starting_layer_height=1
         )
 
+        # now create each of the buttons for the tools
+
+        button_scale = 0.75
+        button_size = (height * button_scale, height * button_scale)
+        button_y = (height - (height * button_scale)) / 2
+        button_x_spacing = 10
+
+        # pen
+        self.pen_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(((button_size[0] + button_x_spacing) * 1, button_y), button_size),
+            text='pen',
+            manager=ui_manager,
+            container = self.panel
+        )
+
+        # eraser
+        self.pen_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(((button_size[0] + button_x_spacing) * 2, button_y), button_size),
+            text='eraser',
+            manager=ui_manager,
+            container = self.panel
+        )
+
 # initialize the toolbar
-toolbar = Toolbar(ui_manager)
+toolbar = Toolbar(ui_manager, 100)
 
 
 clock = pygame.time.Clock()
