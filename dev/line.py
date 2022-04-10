396cb31f19c0f034e262fd9380e86744ee356779
yoctometric <willcunningham64@gmail.com>
2022-04-10 12:47:47 -0400

    started line drawing implementation

--- dev/line.py
+++ dev/line.py
@@ -15,6 +15,7 @@ class LineManager:
         self.lines = []
         self.gridOffset = (0, 0) # set in render call, taken from grid
         self.screenDimensions = screenDimensions # mostly stored for tool actions
+        self.lineEditing = None # th eline that is being placed
 
 
     def updateLines(self) -> None:
@@ -29,13 +30,39 @@ class LineManager:
 
 
     # add line. returns true if valid placement, false if placement failed
+    # if a line is currently being added to, instead adds a point to that line
     def addLine(self, pos: tuple) -> bool:
-        # if there is already a line, return
-        if self.getLineAt(pos):
+        # if there is already a line, (and not line we are editing rn) return
+        overlapping = self.getLineAt(pos)
+        if overlapping:
+            # user clicked back on start line, finish line
+            if overlapping == self.lineEditing:
+                self.completeLine(pos)
+
             return
         
-        line = Line(pos, self.gridOffset)
-        self.lines.append(line)
+
+        # if there is no line being edited right now, place new line and set as line to edit.
+        if self.lineEditing is None:
+            line = Line(pos, self.gridOffset)
+            self.lines.append(line)
+            self.lineEditing = line
+        # otherwise, append point to line!
+        else:
+            self.lineEditing.addPointFromWorldspace(pos, self.gridOffset)
+
+
+    # called to complete the placement of a line
+    # if pos is equal to the start pos of the line, mark the line as closed
+    def completeLine(self, pos: tuple):
+        print("completed line")
+        startPoint = self.lineEditing.getPathScreenSpace(self.gridOffset)[0]
+        print(startPoint, pos)
+        if startPoint  == pos:
+            self.lineEditing.lineIsClosed = True
+            print("equal")
+
+        self.lineEditing = None
     
 
     # checks for if a line is already occupying a space
@@ -74,6 +101,7 @@ class Line:
         self.quality = 1
 
         self.addPoint((0, 0))
+        self.lineIsClosed = False
 
 
     # moves the orbs across the line
@@ -83,9 +111,16 @@ class Line:
 
 
     # adds a point to the line path
-    def addPoint(self, point):
+    def addPoint(self, point: tuple) -> None:
         self.path.append(point)
 
+
+    # adds a point from world space (used for line palcement)
+    def addPointFromWorldspace(self, pos: tuple, gridOffset: tuple) -> None:
+        p = (pos[0] + self.initialGridOfset[0] - gridOffset[0] - self.basePos[0],
+            pos[1] + self.initialGridOfset[1] - gridOffset[1] - self.basePos[1])
+       
+        self.addPoint(p)
 
     # adds an orb to the line path
     def addOrb(self, orb):
@@ -96,12 +131,16 @@ class Line:
     def renderLine(self, window_surface: pygame.Surface, gridOffset: tuple) -> None:
         xOff, yOff = (gridOffset[0] - self.initialGridOfset[0], gridOffset[1] - self.initialGridOfset[1])
 
+        pointsSP = self.getPathScreenSpace(gridOffset)
+        if len(pointsSP) > 1:
+            pygame.draw.lines(window_surface, '#00FE10', self.lineIsClosed, pointsSP, width=10)
+        else:
         # temp render
-        pygame.draw.circle(
-            surface=window_surface, color='#00FE10', 
-            center=(self.basePos[0] + xOff, self.basePos[1] + yOff),
-            radius=10, width=0
-        )
+            pygame.draw.circle(
+                surface=window_surface, color='#00FE10', 
+                center=(self.basePos[0] + xOff, self.basePos[1] + yOff),
+                radius=10, width=0
+            )
 
     
     def getPath(self) -> list:
